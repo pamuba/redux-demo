320fd5bac8feb3c6c4cda46649d0cf21354ca91b
pamuba <partha.bora24@gmail.com>
2021-08-11 20:13:48 +0530

    352

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
+           <ItemContainer cake/>
+           <ItemContainer />
            <CakeContainer/>
            <HooksCakeContainer/>
            <IceCreamContainer></IceCreamContainer>
+           <NewCakeContainer></NewCakeContainer>
         </header>
       </div>
     </Provider>
