46dc75d869eab4df8207d8a7e655fee061e190c8
pamuba <partha.bora24@gmail.com>
2021-08-11 21:03:38 +0530

    356

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
-           <ItemContainer cake/>
+           {/* <ItemContainer cake/>
            <ItemContainer />
            <CakeContainer/>
            <HooksCakeContainer/>
            <IceCreamContainer></IceCreamContainer>
-           <NewCakeContainer></NewCakeContainer>
+           <NewCakeContainer></NewCakeContainer> */}
+           <UserContainer></UserContainer>
         </header>
       </div>
     </Provider>
